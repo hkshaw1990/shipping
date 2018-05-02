1f6d5362b4e8d0abbf4386bb8d271aafd46f0688
Hemant Kumar <hemant@linux.vnet.ibm.com>
2018-05-02 21:01:58 +0530

    Add support for ppc64le

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,7 +24,7 @@ else
 fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
-$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
+$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven hkshaw/maven:3.5.3-jdk-8 mvn -DskipTests package
 
 cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
 
